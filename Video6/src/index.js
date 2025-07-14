b2021bc34d09dcf27c6d4aaeb014e4aada644250
ATRIKA SHOW <readeratrika08@gmail.com>
2025-07-14 11:48:23 +0530

    Add Express app setup and MongoDB connection logic

--- Video6/src/index.js
+++ Video6/src/index.js
@@ -1,10 +1,25 @@
 console.log("hello world");
 
+import { app } from "./app.js";
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 connectDB()
     .then(()=>{
         console.log("Database connection established successfully.");
+
+        // before starting the server, check is there any error 
+        app.on('error', (err)=>{
+            console.error("Error occurred in the application:", err);
+        })
+        // Start the server or perform other operations here
+        app.listen(PORT, ()=>{
+            console.log(`Server is running on port ${PORT}`);
+        })
 
     })
     .catch((error)=>{
