68849b25706b9cf0b374c62dfce1dfcc5e4dee7e
ATRIKA SHOW <readeratrika08@gmail.com>
2025-07-15 12:59:19 +0530

    Add ApiError, ApiResponse, and asyncHandler utility classes for error handling and response management

---
+++ Video6/src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+ const asyncHandler = (requestHandler) => {
+        (req, res, next)=>{
+            Promise.resolve(requestHandler(req, res, next))
+            .catch((err)=>{
+                // If an error occurs, we catch it and pass it to the next middleware
+                next(err);
+            })
+        }
+ }
+ export { asyncHandler };
+ 
+
+
+// 2nd way of writing asyncHandler is
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//                  success: false,
+//             message: error.message || "Custom Error"
+//         })
+//     }
+// };
+
+// export { asyncHandler };
