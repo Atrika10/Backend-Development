a2c0c6de6c94bac5e78f3fa1a97eaa616aec4d08
ATRIKA SHOW <readeratrika08@gmail.com>
2025-10-08 16:42:04 +0530

    Refactor user routes to correct HTTP method for current user endpoint; add update account details, avatar, and cover image routes; include watch history route.

--- Video6/src/routes/user.routes.js
+++ Video6/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { logOutUser, userLogin, userRegister, generateAccessToken, changeCurrentPassword, getCurrentUser } from "../controllers/user.controller.js";
+import { logOutUser, userLogin, userRegister, generateAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateAvatar, updateCoverImage, getUserChannelProfile } from "../controllers/user.controller.js";
 const userRouter = Router();
 import { upload } from "../middlewares/multer.middleware.js"; 
 import { authMiddleware } from "../middlewares/auth.middleware.js";
@@ -22,6 +22,13 @@ userRouter.route("/login").post(userLogin);
 userRouter.route("/logout").post(authMiddleware, logOutUser);
 userRouter.route("/refresh-token").post(generateAccessToken);
 userRouter.route("/change-password").post(authMiddleware, changeCurrentPassword);
-userRouter.route("/current-user").post(authMiddleware, getCurrentUser);
+userRouter.route("/current-user").get(authMiddleware, getCurrentUser);
+
+userRouter.route("/update-account-details").patch(authMiddleware,updateAccountDetails);
+userRouter.route("/update-avatar").patch(authMiddleware, upload.single("avatar"), updateAvatar);
+userRouter.route("/update-cover-image").patch(authMiddleware, upload.single('coverImage'), updateCoverImage);
+
+userRouter.route("/channel/:userName").get(authMiddleware, getUserChannelProfile);
+userRouter.route("/watch-history").get(authMiddleware, getUserChannelProfile); 
 
 export default userRouter;
