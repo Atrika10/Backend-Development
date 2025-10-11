dcd88bdca944e1f4182f6bfa5415810b5482c737
ATRIKA SHOW <readeratrika08@gmail.com>
2025-10-11 14:22:36 +0530

    Fix user password verification and ensure account details update is awaited for proper execution

--- Video6/src/controllers/user.controller.js
+++ Video6/src/controllers/user.controller.js
@@ -134,7 +134,7 @@ const userLogin = asyncHandler(async (req, res) => {
         throw new ApiError(404, "User not found");
     }
     // check password is correct or not
-    const isPasswordCorrect = user.isPasswordCorrect(password);
+    const isPasswordCorrect = await user.isPasswordCorrect(password);
     if (!isPasswordCorrect) {
         throw new ApiError(401, "Invalid credentials");
     }
@@ -296,7 +296,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required");
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
