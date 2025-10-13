793e1fc7fe014ab1186270f8c650b8e94f4128de
ATRIKA SHOW <readeratrika08@gmail.com>
2025-10-13 16:13:25 +0530

    Implement tweet creation functionality with content validation

--- Video6/src/controllers/tweet.controller.js
+++ Video6/src/controllers/tweet.controller.js
@@ -7,6 +7,19 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const userId = req.user._id;
+    const {content} = req.body;
+    if(!content) throw new ApiError(400, "Content is required");
+
+    const tweet = await Tweet.create({
+        owner : userId,
+        content
+    })
+
+    console.log("Tweet has been creted", tweet);
+    return res.status(200).json(
+        new ApiResponse(200, "Tweet has been creted", tweet)
+    )
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
